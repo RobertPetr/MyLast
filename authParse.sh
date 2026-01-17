c2b14b1637e13735f225149b75b1c1dc678cabc8
RobertPetr <robert-petronel.ababei@s.unibuc.ro>
2026-01-17 20:40:14 +0200

    bug fixing

--- authParse.sh
+++ authParse.sh
@@ -3,32 +3,30 @@
 filepath="/var/log/auth.log"
 b="$1"
 
-cat tac "$filepath" | while IFS= read -r linie; do
-    if [[ "b" == "/var/log/btmp" ]]; then
-        if grep -q "Failed password" "$linie"; then
-            read -r luna zi ora host proces mesaj <<< "$linie"
-            read -r failed password for user _ <<< "$mesaj"
-            data=$(date -d "$luna $zi $ora" +"%s")
-            printf "1,%s,%s,%s,%ld,%ld" "$user" "SSH" "$host" "$data" "0"
+while IFS= read -r linie; do
+    IFS=' ' read -r data host command message <<< "$linie"
+
+    data=$(date -d "$data" +"%s")
+
+    user=""
+    line=""
+    flag=0
+
+    if [[ "$command" == *"gdm-password"* && "$b" == "/var/log/wtmp" ]]; then
+        if [[ "$message" == *"session opened"* ]]; then
+            user=$(echo "$message" | sed -n 's/.*user \([^ ]*\)(uid=[0-9]*).*/\1/p')
+            line="tty2"
+            flag=1
+        elif [[ "$message" == *"session closed"* ]]; then
+            echo "a"
+            user="reboot"
+            line="~"
+            flag=1
         fi
-        if grep -q "Invalid user" "$linie"; then
-            read -r luna zi ora host proces mesaj <<< "$linie"
-            read -r invalid u user _ <<< "$mesaj"
-            data=$(date -d "$luna $zi $ora" +"%s")
-            printf "1,%s,%s,%s,%ld,%ld" "$user" "SSH" "$host" "$data" "0"
-        fi
-    else
-        if grep -q "Accepted" "$linie"; then
-            read -r luna zi ora host proces mesaj <<< "$linie"
-            read -r Accepted password for user _ <<< "$mesaj"
-            data=$(date -d "$luna $zi $ora" +"%s")
-            printf "1,%s,%s,%s,%ld,%ld" "$user" "SSH" "$host" "$data" "0"
-        fi
-        if grep -q "login" "$linie"; then
-            read -r luna zi ora host proces mesaj <<< "$linie"
-            read -r pam_unix session opened for u user _ <<< "$mesaj"
-            data=$(date -d "$luna $zi $ora" +"%s")
-            printf "1,%s,%s,%s,%ld,%ld" "$user" "SSH" "$host" "$data" "0"
-        fi
+    elif [[ "$command" == *"unix_chkpwd"* && "$b" == "/var/log/btmp" ]]; then
+        user=$(echo "$message" | awk '{print $6}' | sed 's/[()]//g')
+        line="seat0"
+        flag=1
     fi
-done
+    printf "%d,%s,%s,%s,%ld,0\n" "$flag" "$user" "$line" "$host" "$data"
+done < <(tac "$filepath")
